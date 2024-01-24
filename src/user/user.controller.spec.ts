8230bf0bb418dc5c604ff2bebd574ce127d62001
w8385 <w8385@naver.com>
2024-01-24 19:54:48 +0900

    test: getBojInfoByHandle

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { HttpService } from '@nestjs/axios';
+import { RedisClientType } from 'redis';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
+  let httpService: HttpService;
+  let redis: RedisClientType;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-      providers: [UserService],
-    }).compile();
-
-    controller = module.get<UserController>(UserController);
+    userService = new UserService(httpService, redis);
+    userController = new UserController(userService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getBojInfoByHandle', () => {
+    it('should return {handle, problems}', async () => {
+      const result = {
+        handle: 'w8385',
+        problems: [1000, 1001, 1002, 1003, 1004, 1005],
+      };
+      jest
+        .spyOn(userService, 'getBojInfoByHandle')
+        .mockImplementation(async () => result);
+
+      expect(await userController.getBojUserFromBoj('w8385')).toBe(result);
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { DatabaseModule } from '../database/database.module';
+import { HttpModule } from '@nestjs/axios';
+import { UserController } from './user.controller';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, HttpModule],
+      controllers: [UserController],
       providers: [UserService],
     }).compile();
 
     service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getBojInfoByHandle', () => {
+    it('should return {handle, problems}', async () => {
+      const result = {
+        handle: 'w8385',
+        problems: [1000, 1001, 1002, 1003, 1004, 1005],
+      };
+      jest
+        .spyOn(service, 'getBojInfoByHandle')
+        .mockImplementation(async () => result);
+
+      expect(await service.getBojInfoByHandle('w8385')).toBe(result);
+    });
   });
 });
