a9c1637e7be3373df0bcc4f2f7dc93ec0515f258
w8385 <w8385@naver.com>
2024-01-24 22:26:53 +0900

    test: controller.spec 삭제

--- src/poker/poker.service.spec.ts
+++ src/poker/poker.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PokerService } from './poker.service';
 import { DatabaseModule } from '../database/database.module';
 import { HttpModule } from '@nestjs/axios';
+import { UserService } from '../user/user.service';
 
 describe('PokerService', () => {
   let service: PokerService;
@@ -9,13 +10,25 @@ describe('PokerService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [DatabaseModule, HttpModule],
-      providers: [PokerService],
+      providers: [PokerService, UserService],
     }).compile();
 
     service = module.get<PokerService>(PokerService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getPoker', () => {
+    it('should return {poker}', async () => {
+      const result = [
+        {
+          id: 1,
+          name: 'test',
+          updatedAt: '2021-08-01T00:00:00.000Z',
+          participants: [1, 2, 3, 4, 5, 6],
+        },
+      ];
+      jest.spyOn(service, 'getAll').mockImplementation(async () => result);
+
+      expect(await service.getAll()).toBe(result);
+    });
   });
 });
