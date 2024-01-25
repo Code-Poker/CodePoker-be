918db1d462ce5e793067a947289718cc7e29c227
w8385 <w8385@naver.com>
2024-01-25 15:53:29 +0900

    chore: lint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,26 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 @ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiExcludeEndpoint()
   @ApiOperation({ summary: 'BOJ 유저 정보 가져오기' })
   @Get('info/:handle')
   getBojUserFromBoj(@Param('handle') handle: string) {
     return this.userService.getBojInfoByHandle(handle);
   }
 
+  @ApiExcludeEndpoint()
   @Post('set/:handle')
   setBojUserToRedis(@Param('handle') handle: string) {
     return this.userService.setUser(handle);
   }
 
+  @ApiExcludeEndpoint()
   @Get('get/:handle')
   getBojUserFromRedis(@Param('handle') handle: string) {
     return this.userService.getUser(handle);
