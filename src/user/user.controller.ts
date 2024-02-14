d429ac7199d3c6b205062865795fd2a4ea935278
w8385 <w8385@naver.com>
2024-02-15 04:43:56 +0900

    feat: user/calc 삭제

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { UserService } from './user.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 @ApiTags('User')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @ApiOperation({ summary: '포커 결과 자기꺼만 계산하기' })
-  @Get(':pokerId/calc/:handle')
-  calcScore(
-    @Param('pokerId') pokerId: string,
-    @Param('handle') handle: string,
-  ) {
-    return this.userService.calcScore(pokerId, handle);
-  }
-}
+export class UserController {}
