1aea4e6efb93429045f8f745c24eb85a9593d04a
w8385 <w8385@naver.com>
2024-04-12 01:48:12 +0900

    cd: SSH 유저 변경

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configCodePoker, configSSUJoon } from './config.swagger';
 import { HttpExceptionFilter } from './http-exception.filter';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,7 @@ async function bootstrap() {
   configCodePoker(app);
   configSSUJoon(app);
 
-  await app.listen(6371);
+  await app.listen(process.env.PORT);
 }
 
 bootstrap().then(() => {
