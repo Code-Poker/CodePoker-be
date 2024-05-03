07d7e7be7f0c9666e8ab5d1b863124ab85814db0
w8385 <w8385@naver.com>
2024-05-03 19:40:26 +0900

    feat: cache `on`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { BojModule } from './boj/boj.module';
 import { DatabaseModule } from './database/database.module';
 import { GroupModule } from './group/group.module';
 import { PokerModule } from './poker/poker.module';
 import { ProblemModule } from './problem/problem.module';
+import { SolvedModule } from './solved/solved.module';
 import { SsuModule } from './ssu/ssu.module';
 import { UserModule } from './user/user.module';
-import { SolvedModule } from './solved/solved.module';
 
 @Module({
   imports: [
-    // CacheModule.register({
-    //   ttl: 1000 * 10,
-    //   max: 10,
-    // }),
+    CacheModule.register({
+      ttl: 1000 * 10,
+      max: 10,
+    }),
     UserModule,
     PokerModule,
     DatabaseModule,
@@ -25,10 +27,10 @@ import { SolvedModule } from './solved/solved.module';
     SolvedModule,
   ],
   providers: [
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useClass: CacheInterceptor,
-    // },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule {}
