ba929196529cddccda9d53c5e3f041c1c678abc8
w8385 <w8385@naver.com>
2024-05-03 19:13:11 +0900

    feat: ultimate WD recipe

--- src/webdriver/webdriver.provider.ts
+++ src/webdriver/webdriver.provider.ts
@@ -5,7 +5,19 @@ export const webdriverProvider = {
   useFactory: async () => {
     return remote({
       capabilities: {
+        webSocketUrl: true,
         browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: [
+            '--headless',
+            '--single-process',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--no-sandbox',
+            '--disable-javascript',
+            'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+          ],
+        },
       },
     });
   },
