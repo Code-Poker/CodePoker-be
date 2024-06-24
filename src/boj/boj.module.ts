ff39c2fb007178c8829269991761748d7780f9bb
박근형 <me@w8385.dev>
2024-06-25 07:41:13 +0900

    refactor: remove webdriver

--- src/boj/boj.module.ts
+++ src/boj/boj.module.ts
@@ -1,12 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
-import { WebdriverModule } from '../webdriver/webdriver.module';
 import { BojController } from './boj.controller';
 import { BojRepository } from './boj.repository';
 import { BojService } from './boj.service';
 
 @Module({
-  imports: [WebdriverModule],
+  imports: [HttpModule],
   controllers: [BojController],
   providers: [BojService, BojRepository],
   exports: [BojService],
